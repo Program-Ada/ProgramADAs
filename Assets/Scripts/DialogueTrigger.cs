0b790d531b6fffc7b42213a5e139bc97cadb66d2
eduarda <eduarda.nunes@estudante.ufjf.br>
2023-11-13 18:05:52 -0300

    Trigger por proximidade quase pronto (bug com box collider)

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -6,8 +6,28 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+    public bool playerIsClose;
 
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.E) && playerIsClose){
+            TriggerDialogue();
+        }
+    }
     public void TriggerDialogue(){
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("Player")){
+            playerIsClose = true;
+            Debug.Log("true");
+        }
+    }
+
+        private void OnTriggerExit2D(Collider2D other){
+        if(other.CompareTag("Player")){
+            playerIsClose = false;
+            Debug.Log("false");
+        }
     }
 }
