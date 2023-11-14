d7fca404653ffddeafcece897e1690c254b94993
Duda Nunes <eduardapmn@gmail.com>
2023-11-14 01:43:38 -0300

    Chat por proximidade, E para iniciar conversa e para continuar o diálogo, dialogo fecha ao se afastar do npc

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -53,7 +53,7 @@ public class DialogueManager : MonoBehaviour{
         }
     }
 
-    void EndDialogue(){
+    public void EndDialogue(){
         animator.SetBool("IsOpen", false);
         Debug.Log("Conversa Finalizada.");
     }
--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -7,27 +7,31 @@ public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
     public bool playerIsClose;
+    public bool playerIsChatting;
 
     void Update(){
-        if(Input.GetKeyDown(KeyCode.E) && playerIsClose){
+        if(Input.GetKeyDown(KeyCode.E) && playerIsClose && playerIsChatting == false){
             TriggerDialogue();
+        }else if(Input.GetKeyDown(KeyCode.E) && playerIsClose && playerIsChatting){
+            FindObjectOfType<DialogueManager>().DisplayNextSentence();
         }
     }
     public void TriggerDialogue(){
+        playerIsChatting = true;
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 
     private void OnTriggerEnter2D(Collider2D other){
         if(other.CompareTag("Player")){
             playerIsClose = true;
-            Debug.Log("true");
         }
     }
 
         private void OnTriggerExit2D(Collider2D other){
         if(other.CompareTag("Player")){
             playerIsClose = false;
-            Debug.Log("false");
+            playerIsChatting = false;
+            FindObjectOfType<DialogueManager>().EndDialogue();
         }
     }
 }
