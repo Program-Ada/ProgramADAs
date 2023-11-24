dfa9c15689e1cd727f48a5a6aa7ad1add3f69e94
Duda Nunes <eduardapmn@gmail.com>
2023-11-24 16:34:20 -0300

    Quiz Questions and Changed Barbara's dialogue

--- Assets/Scripts/QuestionAndAnswers.cs
+++ Assets/Scripts/QuestionAndAnswers.cs
@@ -1,7 +1,9 @@
+using UnityEngine;
 
 [System.Serializable]
 public class QuestionAndAnswers
 {
+    [TextArea(3,10)]
     public string Question;
     public string[] Answers;
     public int correctAnswer;
