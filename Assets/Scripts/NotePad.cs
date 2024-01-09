1e4b63dd377a02aacf3857d58dcd76c288755988
Victoria <victoria.yamashita@estudante.ufjf.br>
2024-01-09 19:57:29 -0300

    initial struct

---
+++ Assets/Scripts/NotePad.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotePad : MonoBehaviour
+{
+    public GameObject NotePadCanvas;
+    public GameObject Player;
+
+    public GameObject Menu;
+    public GameObject Chapter1;
+    public GameObject Chapter2;
+    void Start()
+    {
+        NotePadCanvas.SetActive(false); 
+        Menu.SetActive(true);
+        Chapter1.SetActive(false);
+        Chapter2.SetActive(false);
+    }
+
+    public void OpenNotePad() {
+        Player.GetComponent<PlayerMovement>().enabled = false;
+        NotePadCanvas.SetActive(true);   
+    }
+
+    public void Screen_Menu() {
+        Menu.SetActive(true);
+        Chapter1.SetActive(false);
+        Chapter2.SetActive(false);    
+    }
+
+    public void Screen_Chapter1() {
+        Menu.SetActive(false);  
+        Chapter1.SetActive(true);
+        Chapter2.SetActive(false);  
+    }
+
+    public void Screen_Chapter2() {
+        Menu.SetActive(false);  
+        Chapter1.SetActive(false);
+        Chapter2.SetActive(true);  
+    }
+}
