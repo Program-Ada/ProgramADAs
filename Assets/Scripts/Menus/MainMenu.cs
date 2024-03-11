765aa3a8dd3420440f1c32ca4e3ebe9f2cbda336
Eduarda Nunes <eduarda.nunes@estudante.ufjf.br>
2024-03-11 02:26:15 -0300

    Organizes Scripts into folders

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -6,7 +6,11 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame(){
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("GameSelection");
+    }
+
+    public void GameOptions(){
+        SceneManager.LoadScene("GameOptions");
     }
 
     public void QuitGame(){
