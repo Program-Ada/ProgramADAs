8eedb216a661ea935e477e8491370a0a19a2a4c2
Eduarda Nunes <eduarda.nunes@estudante.ufjf.br>
2024-05-28 20:13:13 -0300

    New UI Sprites for Tutorial, Quiz, QuisStart, QuizScore, EscMenu and DialogueBox

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -9,12 +9,21 @@ public class MouseManager : MonoBehaviour
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot = Vector2.zero;
 
-    void OnMouseDown() {
-      Debug.Log("Mouse Down");
+    /*void OnMouseDown() {
       Cursor.SetCursor(cursorClick, hotSpot, cursorMode); 
     }
 
     void OnMouseUp() {
       Cursor.SetCursor(cursorDefault, hotSpot, cursorMode); 
+    }*/
+
+    void Update(){
+      
+      if(Input.GetMouseButtonDown(0)){
+        Cursor.SetCursor(cursorClick, hotSpot, cursorMode);  
+      }
+      if(Input.GetMouseButtonUp(0)){
+        Cursor.SetCursor(cursorDefault, hotSpot, cursorMode);  
+      }
     }
 }
