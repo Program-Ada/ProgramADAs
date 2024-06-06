e97b66f3f364e96b5bc6636bf9cac243b7bbfecc
Eduarda Nunes <eduarda.nunes@estudante.ufjf.br>
2024-06-05 21:46:41 -0300

    NotePad Package

---
+++ Assets/Scripts/NotePad/Chapter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Chapter 
+{
+    // [TextArea(3,10)]
+    public string tittle;
+    [TextArea(3,10)]
+    public string content;
+
+    // public bool chapterOpen;
+
+    // int currentpage = 1;
+}
