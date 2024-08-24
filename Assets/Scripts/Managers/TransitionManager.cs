7d6ed8e974dc735c860ccce40cd4e9f60f7de7cc
Eduarda Nunes <eduarda.nunes@estudante.ufjf.br>
2024-08-24 03:12:39 -0300

    Started Transition Animation Script (Infinit Animation Bug)

--- Assets/Scripts/Managers/TransitionManager.cs
+++ Assets/Scripts/Managers/TransitionManager.cs
@@ -9,18 +9,20 @@ public class TransitionManager : MonoBehaviour
     public Animator topBlackBar;
     public GameObject bottomBlackBar;
 
-private void OnEnable() {
-    SceneManager.sceneLoaded += OnSceneLoaded;
-}
-private void OnDisable() {
-    SceneManager.sceneUnloaded += OnSceneUnloaded;
-}
+    private void OnEnable() {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    private void OnDisable() {
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
+    }
 
-private void OnSceneLoaded(Scene scene, LoadSceneMode loadScene){
-    transitionBackground.SetBool("EnterScene", true);
-}
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadScene){
+        Debug.Log("Loaded");
+        transitionBackground.SetBool("EnterScene", true);
+    }
 
-private void OnSceneUnloaded(Scene scene){
-    transitionBackground.SetBool("EnterScene", false);
-}
+    private void OnSceneUnloaded(Scene scene){
+        Debug.Log("Unloaded");
+        transitionBackground.SetBool("EnterScene", false);
+    }
 }
