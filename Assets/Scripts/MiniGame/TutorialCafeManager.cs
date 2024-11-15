7941c0c91ea518de3fcb1adedd80e092907e1151
Eduarda Nunes <eduarda.nunes@estudante.ufjf.br>
2024-11-15 03:16:16 -0300

    Tutorial Display Next and Previous Steps Functions

--- Assets/Scripts/MiniGame/TutorialCafeManager.cs
+++ Assets/Scripts/MiniGame/TutorialCafeManager.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TutorialCafeManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static TutorialCafeManager Instance;
+    public List<GameObject> steps;
+    private int atualStep = -1;
+
     void Start()
     {
-        
+        Instance = this;
+        foreach(Transform child in this.transform){
+            steps.Add(child.gameObject);
+            child.gameObject.SetActive(false); // garantindo que todos estão desabilitados
+        }
+
+        DisplayNextStep();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void DisplayNextStep(){
+        Debug.Log("Next");
+        Debug.Log(atualStep);
+        if(atualStep >= 0){
+            steps[atualStep].SetActive(false);
+        }
+        if(atualStep < steps.Count-1){
+            steps[++atualStep].SetActive(true);
+        }else{
+            ButtonsMiniGame.instance.Start_Btn();
+        }
     }
+
+    public void DisplayPreviousStep(){
+        Debug.Log("Previous");
+        Debug.Log(atualStep);
+        if(atualStep > 0){
+            steps[atualStep--].SetActive(false);
+        }
+        steps[atualStep].SetActive(true);
+    } 
 }
