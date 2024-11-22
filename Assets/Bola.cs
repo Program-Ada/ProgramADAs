d56a21a17da3ca96a1d8d15e2f8934a823cf3daf
luanalauschner <vilarinholuana@gmail.com>
2024-11-22 15:53:27 -0300

    sprites

---
+++ Assets/Bola.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int numero;
+    public bool listrada;
+    public bool impar;
+    public string cor;
+    /*void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
