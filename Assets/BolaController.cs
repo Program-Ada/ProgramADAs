d35e6f61e5e28db993c19a1b511a92105530b58d
luanalauschner <vilarinholuana@gmail.com>
2024-12-06 16:11:21 -0300

    prefab

---
+++ Assets/BolaController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BolaController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int numero;
+    public bool par;
+    public bool listrada;
+    public string cor;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
