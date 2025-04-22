acdc960a139bcf1d011a90639ff35809a7c7241a
luanalauschner <vilarinholuana@gmail.com>
2025-04-22 17:19:32 -0300

    natalia para entrr na fase

---
+++ Assets/Scripts/Fase3/StartFase3.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartFase3 : MonoBehaviour, IDataPersistence
+{
+    public static StartFase3 Instance;
+    public GameObject buttonFalse;
+    public GameObject buttonTrue;
+    public Animator animator;
+    public GameObject nataliaUnlocked;
+    public GameObject nataliaLocked;
+    private bool isLevelUnlocked = false;
+    private bool isTrueClicked = false;
+
+    void Start(){
+        Instance = this;
+
+        nataliaUnlocked.SetActive(false);
+        nataliaLocked.SetActive(false);    
+
+        if(isLevelUnlocked){
+            nataliaUnlocked.SetActive(true);
+        }else{
+            nataliaLocked.SetActive(true);
+        }
+    }
+
+    public void OpenAskMiniGame(){
+        Enable_Buttons();
+        animator.SetBool("IsOpen", true);
+    }
+
+    public void CloseAskMiniGame(){
+        Disable_Buttons();
+        animator.SetBool("IsOpen", false);
+    }
+    
+    public void AnwserFalse() {
+        CloseAskMiniGame();
+    }
+
+    public void AnwserTrue(){
+        isTrueClicked = true;
+        DataPersistenceManager.Instance.SaveGame();
+        CloseAskMiniGame();
+        SceneManager.LoadScene("Fase3");
+    }
+
+    public void Disable_Buttons(){
+        buttonFalse.SetActive(false);
+        buttonTrue.SetActive(false);
+    }
+
+    public void Enable_Buttons(){
+        buttonFalse.SetActive(true);
+        buttonTrue.SetActive(true);
+    }
+
+    public void LoadData(GameData data){
+        //colcar a Fase3 no unlocked Fase e mudar o mudar o i
+        if(data.unlockedFases[1]){
+            isLevelUnlocked = true;
+        }
+    }
+    public void SaveData(ref GameData data){
+        if(isTrueClicked){
+            isTrueClicked = false;
+            //entender o que é esse questProgressIndex
+            data.questProgressIndex = 5;
+        }
+    }
+}
